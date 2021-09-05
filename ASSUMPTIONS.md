9c0990104b369d403aef1a2d2a215100c56f3b8e
Vasile Furdui <vasiafurdui@yahoo.com>
2021-09-05 20:57:09 +0300

    Create calculator app

---
+++ ASSUMPTIONS.md
@@ -0,0 +1,11 @@
+For this task I will create an angular application
+Created first task with ng new calculator
+I will create html, css.
+The following steps was done: 
+1. Create buttons for each number, operation, action.
+2. **allClear()** a function to clear everything
+3. **pressKey(key: string)** a function to handle click event on numbers and operators
+4. **getAnswer()** a function to handle click on equal button
+5. **RandomNumberService** for generating random number
+6. **rand()** function to handle random number generation
+7. I divided the main display in two in order to show current operation and past operations.
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {RandomNumberService} from './random-number.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,87 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'calculator';
+  title = 'angular-calculator-app';
+  subDisplayText = '';
+  mainDisplayText = '';
+  operand1: number = null;
+  operand2: number = null;
+  operator = '';
+  calculationString = '';
+  // This string  denotes the operation being performed
+  answered = false;
+  //  flag to check whether the solution has been processed
+  operatorSet = false;
+  lastExpression = [];
+
+  constructor(private readonly randomNumberService: RandomNumberService) {
+  }
+
+  pressKey(key: string): void {
+    if (key === '/' || key === 'x' || key === '-' || key === '+') {
+      const lastKey = this.mainDisplayText[this.mainDisplayText.length - 1];
+      if (lastKey === '/' || lastKey === 'x' || lastKey === '-' || lastKey === '+') {
+        this.operatorSet = true;
+      }
+      if ((this.operatorSet) || (this.mainDisplayText === '')) {
+        return;
+      }
+      this.operand1 = parseFloat(this.mainDisplayText);
+      this.operator = key;
+      this.operatorSet = true;
+    }
+    if (this.mainDisplayText.length === 10) {
+      return;
+    }
+    this.mainDisplayText += key;
+  }
+
+  allClear(): void {
+    this.mainDisplayText = '';
+    this.subDisplayText = '';
+    this.operatorSet = false;
+    this.answered = false;
+    this.operand1 = null;
+    this.operand2 = null;
+  }
+
+  getAnswer(): void {
+    this.calculationString = this.mainDisplayText;
+    this.operand2 = parseFloat(this.mainDisplayText.split(this.operator)[1]);
+    if (this.operator === '/') {
+      this.mainDisplayText = (this.operand1 / this.operand2).toString();
+      if (this.mainDisplayText.length > 9) {
+        this.mainDisplayText = this.mainDisplayText.substr(0, 9);
+      }
+      this.lastExpression.push(this.operand1 + ' / ' + this.operand2 + ' = ' + (this.operand1 / this.operand2));
+    } else if (this.operator === 'x') {
+      this.mainDisplayText = (this.operand1 * this.operand2).toString();
+      this.lastExpression.push(this.operand1 + ' * ' + this.operand2 + ' = ' + (this.operand1 * this.operand2));
+      if (this.mainDisplayText.length > 9) {
+        this.mainDisplayText = 'ERROR';
+        this.subDisplayText = 'Range Exceeded';
+      }
+    } else if (this.operator === '-') {
+      this.mainDisplayText = (this.operand1 - this.operand2).toString();
+      this.lastExpression.push(this.operand1 + ' - ' + this.operand2 + ' = ' + (this.operand1 - this.operand2));
+    } else if (this.operator === '+') {
+      this.mainDisplayText = (this.operand1 + this.operand2).toString();
+      this.lastExpression.push(this.operand1 + ' + ' + this.operand2 + ' = ' + (this.operand1 + this.operand2));
+      if (this.mainDisplayText.length > 9) {
+        this.mainDisplayText = 'ERROR';
+        this.subDisplayText = 'Range Exceeded';
+      }
+    } else {
+      this.subDisplayText = 'ERROR: Invalid Operation';
+    }
+    this.answered = true;
+  }
+
+  rand(): void {
+    this.randomNumberService.getRandomNumber().subscribe(
+      (generatedNumber: number) => {
+          this.mainDisplayText += generatedNumber;
+        }
+    );
+  }
 }
